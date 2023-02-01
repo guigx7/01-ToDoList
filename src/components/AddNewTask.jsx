379d5b4c4bea9bd9d2f8480a3bef6a7641df085e
Guilherme Tavares <guigo211103@gmail.com>
2023-02-01 11:46:41 -0300

    Feito o componente AddNewTask

--- src/components/AddNewTask.jsx
+++ src/components/AddNewTask.jsx
@@ -1,10 +1,13 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './AddNewTask.module.css'
 
 export function AddNewTask() {
     return(
         <section>
             <input type='text' placeholder='Adicione uma nova tarefa'/>
-            <button>Criar</button>
+            <button>
+              <span>Criar</span> <PlusCircle size={16} weight="bold" />
+            </button>
         </section>
     )
 }
